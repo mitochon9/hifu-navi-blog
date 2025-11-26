b957d2e9cf5e536970bb7852b69a9286a567dee0
mitochon9 <mitochon9@gmail.com>
2025-11-27 00:13:41 +0900

    Add database package with Prisma integration, including initial schema, migrations, and TypeScript configuration. Implement database access methods and provide usage instructions in README.

---
+++ packages/database/src/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prismaGlobal: PrismaClient | undefined;
+}
+
+export const prisma: PrismaClient = globalThis.prismaGlobal ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
+
+export type {
+  Prisma,
+  PrismaClient,
+} from "@prisma/client";
